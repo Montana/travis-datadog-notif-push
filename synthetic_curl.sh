eef0c90ad3698ddef7bf8d15fa3946e0eec9034a
Michael Mendy <montana@linux.com>
2024-02-01 14:22:09 -0800

    add `awk` to synthetic curl request

--- synthetic_curl.sh
+++ synthetic_curl.sh
@@ -5,7 +5,7 @@
 DATADOG_EVENT_TITLE="$(echo "$1" | sed 's/[^a-zA-Z0-9._-]/_/g')"
 DATADOG_EVENT_TEXT="$(echo "$2" | sed 's/[^a-zA-Z0-9._-]/_/g')"
 DATADOG_EVENT_PRIORITY="$(echo "$3" | sed 's/[^a-zA-Z0-9._-]/_/g')"
-DATADOG_EVENT_TAGS="$(echo "$4" | sed -r 's/[^a-zA-Z0-9.,_-]/_/g')"
+DATADOG_EVENT_TAGS="$(echo "$4" | awk 'BEGIN{RS=","; ORS=","} {gsub(/[^a-zA-Z0-9._-]/, "_"); print}')"
 DATADOG_EVENT_ALERT_TYPE="$(echo "$5" | sed 's/[^a-zA-Z0-9._-]/_/g')"
 
 curl -X POST -H "Content-type: application/json" -d "{
